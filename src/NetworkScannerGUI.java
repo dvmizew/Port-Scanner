e5056f9040bf8183283fa7c4bf148ad41f791044
Daniel Radu <89708521+dvmizew@users.noreply.github.com>
2024-06-03 15:42:26 +0300

    removed unnecessary block of code

--- src/NetworkScannerGUI.java
+++ src/NetworkScannerGUI.java
@@ -157,11 +157,4 @@ public class NetworkScannerGUI extends JFrame {
             }
         }
     }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            NetworkScannerGUI app = new NetworkScannerGUI();
-            app.setVisible(true);
-        });
-    }
 }
